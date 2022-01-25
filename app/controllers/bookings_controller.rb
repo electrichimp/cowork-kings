6d74b74eb37329cac23c58a421751a41eac657a5
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-25 16:58:47 -0500

    method destroy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: ["destroy"]
   def create
     @booking = Booking.new(booking_params)
     @booking.coworking = Coworking.find(params[:coworking_id])
@@ -11,12 +12,18 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @booking.destroy
+    redirect_to my_visits_path
   end
 
   def confirm
   end
 
   private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
