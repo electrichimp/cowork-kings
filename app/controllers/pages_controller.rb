7cb589d2180fbbcfc5b5c9c43cd07170ee9cf930
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-25 17:05:14 -0500

    controlador my_coworkings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,14 @@ class PagesController < ApplicationController
     @coworkings = Coworking.all
   end
 
+  def my_coworkings
+    if user_signed_in?
+      @coworkings = current_user.coworkings
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
   def my_visits
     if user_signed_in?
       @bookings = current_user.bookings
