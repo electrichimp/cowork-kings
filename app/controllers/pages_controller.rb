487b889c3abd97a5f799ca20122a41ac318742ba
JUAN MANUEL GOMEZ <jumagom@gmail.com>
2022-01-25 15:11:48 -0500

    limpiando git

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
   def home
+    @coworkings = Coworking.all
+  end
+
+  def my_visits
+    if user_signed_in?
+      @visits = current_user.bookings
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def my_rentals
+    if user_signed_in?
+      @visits = current_user.coworkings
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
