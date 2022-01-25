cef1456d9d1ef4bff4cefded0b55f476b8ffb8b5
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-01-25 15:38:04 -0500

    new, create and show

--- app/controllers/coworkings_controller.rb
+++ app/controllers/coworkings_controller.rb
@@ -1,2 +1,25 @@
 class CoworkingsController < ApplicationController
+  def new
+    @coworking = Coworking.new
+  end
+
+  def create
+    @coworking = Coworking.new(coworking_params)
+    @coworking.user = current_user
+    if @coworking.save
+      redirect_to coworking_path(@coworking)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @coworking = Coworking.find(params[:id])
+  end
+
+  private
+
+  def coworking_params
+    params.require(:coworking).permit(:name, :address, :description, :price, :capacity)
+  end
 end
