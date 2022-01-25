3465492df5a257c66cc5ee3b83a2f2d59a3a663b
JUAN MANUEL GOMEZ <jumagom@gmail.com>
2022-01-25 12:48:56 -0500

    simple seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coworking.destroy_all
+Booking.destroy_all
+
+Coworking.create(
+  name: "Oficina Cuzco",
+  address: "Av El Sol 123",
+  description: "gergdtgrtdgerty45ryrthrtghfgdhfgjhfghjghfjfghfgjfghj",
+  price: 99.50,
+  capacity: 4,
+  user: User.find(1)
+)
+
+Coworking.create(
+  name: "Oficina lima",
+  address: "Av Arenales 123",
+  description: "gergdtgrtdgerty45ryrthrtghfgdhfgjhfghjghfjfghfgjfghj",
+  price: 109.50,
+  capacity: 6,
+  user: User.find(1)
+)
+
+Booking.create(
+  coworking: Coworking.find(1),
+  user: User.find(1),
+  start_date: Date.today,
+  end_date: Date.tomorrow,
+  accepted: false
+)
